bd93f0400a05e752ddae96ea01b0f9f94cce4870
NJ <147250418+Agent-X44@users.noreply.github.com>
2025-10-17 15:10:25 +0800

    pickupdate field color changed

--- Client/src/components/Hero.jsx
+++ Client/src/components/Hero.jsx
@@ -104,7 +104,7 @@ const Hero = () => {
               type="date"
               id="pickup-date"
               min={new Date().toISOString().split('T')[0]}
-              className="text-gray-800 border border-gray-300 rounded-lg px-3 py-2 w-full md:w-auto focus:outline-primary focus:border-primary"
+              className="text-gray-400 border border-gray-300 rounded-lg px-3 py-2 w-full md:w-auto focus:outline-primary focus:border-primary"
               required
             />
           </div>
