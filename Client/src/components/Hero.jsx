981da7854226f7f786a59c2f68a756344b21b6a7
NJ <147250418+Agent-X44@users.noreply.github.com>
2025-10-25 18:15:46 +0800

    font

--- Client/src/components/Hero.jsx
+++ Client/src/components/Hero.jsx
@@ -79,7 +79,7 @@ const Hero = () => {
                 setIsDropdownOpen(true)
               }}
               onFocus={() => setIsDropdownOpen(true)}
-              className="text-sm text-gray-500 border border-gray-300 rounded-lg px-3 py-2 w-full md:w-48 placeholder:text-gray-400 focus:outline-primary focus:border-primary"
+              className="text-sm text-gray-500 border border-gray-300 rounded-lg px-3 py-2 w-full md:w-auto focus:outline-primary focus:border-primary"
               placeholder="Select or type"
             />
             {isDropdownOpen && filteredCities && (
