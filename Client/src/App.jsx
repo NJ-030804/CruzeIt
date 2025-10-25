08b5b3c46d2fb9192f11754f2a40095d1a563c13
kramogs-bug <vjohnmark312@gmail.com>
2025-10-25 14:34:51 +0800

    Add Coming Soon pages and refactor Navbar logic

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -14,36 +14,48 @@ import ManageCars from './pages/owner/ManageCars'
 import ManageBookings from './pages/owner/ManageBookings'
 import ModeratorDashboard from './components/ModeratorDashboard'
 import Login from './components/Login'
+import ComingSoon from './components/ComingSoon'
 import { Toaster } from 'react-hot-toast'
 import { useAppContext } from './context/AppContext'
 
 const App = () => {
-
-  const {showLogin} = useAppContext()
-  const isOwnerPath = useLocation().pathname.startsWith('/owner')
+  const { showLogin } = useAppContext()
+  const location = useLocation()
+  const isOwnerPath = location.pathname.startsWith('/owner')
   const isModeratorPath = location.pathname.startsWith('/moderator')
-
+  
   return (
     <>
       <Toaster />
       {showLogin && <Login/>}
-      {!isOwnerPath && <Navbar/>}
-
+      {!isOwnerPath && !isModeratorPath && <Navbar/>}
+      
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/car-details/:id' element={<CarDetails />} />
         <Route path='/cars' element={<Cars />} />
         <Route path='/my-bookings' element={<MyBookings />} />
         <Route path='/about' element={<AboutUs />} />
+        
+        {/* Coming Soon Pages */}
+        <Route path='/help' element={<ComingSoon />} />
+        <Route path='/terms' element={<ComingSoon />} />
+        <Route path='/privacy' element={<ComingSoon />} />
+        <Route path='/insurance' element={<ComingSoon />} />
+        <Route path='/cookies' element={<ComingSoon />} />
+        
+        {/* Owner Routes */}
         <Route path='/owner' element={<Layout />}>
           <Route index element={<Dashboard />} />
           <Route path='add-car' element={<AddCar />} />
           <Route path='manage-cars' element={<ManageCars />} />
           <Route path='manage-bookings' element={<ManageBookings />} />
         </Route>
+
+        {/* Moderator Route */}
         <Route path='/moderator' element={<ModeratorDashboard />} />
       </Routes>
-
+      
       {!isOwnerPath && !isModeratorPath && <Footer />}
     </>
   )
